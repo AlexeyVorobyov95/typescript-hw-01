67ff34eb73c6a7f982483937f287cec363bc830d
AlexeyVorobyov95 <forced1395@gmail.com>
2023-05-22 11:41:08 +0300

    add fix

--- src/components/TypeScript/TypeScript.js
+++ src/components/TypeScript/TypeScript.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-vars */
+// eslint-disable-next-line strict
 "use strict";
 /* eslint-disable @typescript-eslint/no-unused-vars */
 Object.defineProperty(exports, "__esModule", { value: true });
